9726e63fc8298838e01ebebd3bbfb0049d722513
YuZhe Wu (巫宇哲) <U01632@unimicron.com>
2025-06-06 15:44:18 +0800

    刪除不需要的檔案

--- routers/api_v1/routers.py
+++
@@ -1,9 +0,0 @@
-# routers/routers.py
-from fastapi import APIRouter
-from routers.api_v1.user import router as user_router
-from routers.api_v1.data import router as data_router
-
-router = APIRouter()
-
-router.include_router(user_router, prefix="/user")
-router.include_router(data_router, prefix="/data")
